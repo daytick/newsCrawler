1d618ac0cdbb78f58ee46ca1674296c384488acb
luyi <luyi95@foxmail.com>
2020-03-26 23:57:06 +0800

    complete rough skeleton

--- src/main/java/top/daytick/newsCrawler/Main.java
+++ src/main/java/top/daytick/newsCrawler/Main.java
@@ -6,27 +6,78 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        try {
-            String html = getIndexPage();
-            System.out.println(html);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public static void main(String[] args) throws IOException {
+        // 链接池
+        Queue<String> linkPool = new LinkedList<>();
+        // 已处理的链接池
+        Set<String> processedPool = new HashSet<>();
+
+        linkPool.offer("https://news.sina.cn");
+
+        while (true) {
+
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if (linkPool.isEmpty()) {
+                break;
+            }
+
+            String link = linkPool.poll();
+            if (link.startsWith("//")) {
+                link = "https:" + link;
+            }
+
+            if (processedPool.contains(link)) {
+                continue;
+            }
+
+            if (link.contains("sina.cn")) {
+
+                Document doc = httpGetAndParseHtml(link);
+                List<Element> aTags = doc.select("a");
+                for (Element aTag : aTags) {
+                    String mayBeUsefulLink = aTag.attr("href");
+                    if (!(mayBeUsefulLink.isEmpty() || mayBeUsefulLink.startsWith("javascript") || mayBeUsefulLink.contains("photo.sina.cn"))) {
+                        linkPool.offer(mayBeUsefulLink);
+                    }
+                }
+
+                // 如果是新闻页，就存入数据库
+                List<Element> articleTags = doc.select("article");
+                if (!articleTags.isEmpty()) {
+                    for (Element articleTag : articleTags) {
+                        String title = articleTag.child(0).text();
+                        System.out.println("title=" + title + "\"");
+                    }
+                }
+
+                processedPool.add(link);
+            }
+
         }
+
     }
 
-    private static String getIndexPage() throws IOException {
+    private static Document httpGetAndParseHtml(String link) throws IOException {
         CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("https://sina.cn");
-        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-            System.out.println(response1.getStatusLine());
-            HttpEntity entity1 = response1.getEntity();
-            return EntityUtils.toString(entity1);
+        HttpGet httpGet = new HttpGet(link);
+        try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
+            System.out.println(response.getStatusLine());
+            HttpEntity entity = response.getEntity();
+            return Jsoup.parse(EntityUtils.toString(entity));
         }
     }
 
