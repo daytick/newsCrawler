b723a16f688c27fbe5546a8a2d4953e6f14f6528
luyi <luyi95@foxmail.com>
2020-03-27 14:23:54 +0800

    design and complete basic algorithm
    also do a little refactoring

--- src/main/java/top/daytick/newsCrawler/Main.java
+++ src/main/java/top/daytick/newsCrawler/Main.java
@@ -1,6 +1,8 @@
 package top.daytick.newsCrawler;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -12,25 +14,19 @@ import org.jsoup.nodes.Element;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
+    private static final Pattern PATTERN = Pattern.compile("https?://\\w*\\.?sina\\.cn.*");
+    private static final String USERAGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36";
 
     public static void main(String[] args) throws IOException {
-        // 链接池
         Queue<String> linkPool = new LinkedList<>();
-        // 已处理的链接池
         Set<String> processedPool = new HashSet<>();
-
-        linkPool.offer("https://news.sina.cn");
+        linkPool.offer("https://sina.cn");
 
         while (true) {
-
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
             if (linkPool.isEmpty()) {
                 break;
             }
@@ -39,41 +35,41 @@ public class Main {
             if (link.startsWith("//")) {
                 link = "https:" + link;
             }
+            System.out.println("link=" + link);
 
             if (processedPool.contains(link)) {
                 continue;
             }
 
-            if (link.contains("sina.cn")) {
-
+            Matcher matcher = PATTERN.matcher(link);
+            if (matcher.matches()) {
                 Document doc = httpGetAndParseHtml(link);
-                List<Element> aTags = doc.select("a");
-                for (Element aTag : aTags) {
-                    String mayBeUsefulLink = aTag.attr("href");
-                    if (!(mayBeUsefulLink.isEmpty() || mayBeUsefulLink.startsWith("javascript") || mayBeUsefulLink.contains("photo.sina.cn"))) {
-                        linkPool.offer(mayBeUsefulLink);
-                    }
-                }
 
-                // 如果是新闻页，就存入数据库
-                List<Element> articleTags = doc.select("article");
-                if (!articleTags.isEmpty()) {
-                    for (Element articleTag : articleTags) {
-                        String title = articleTag.child(0).text();
-                        System.out.println("title=" + title + "\"");
-                    }
-                }
+                doc.select("a").stream().map(aTag -> aTag.attr("href")).filter(mayBeUsefulLink -> mayBeUsefulLink.startsWith("http")).forEach(linkPool::offer);
 
-                processedPool.add(link);
+                storeIntoDatabaseIfItIsNews(doc);
             }
 
+            processedPool.add(link);
+            System.out.print("\n");
         }
 
     }
 
+    private static void storeIntoDatabaseIfItIsNews(Document doc) {
+        List<Element> articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                String title = articleTag.child(0).text();
+                System.out.println("title=" + title + "\"");
+            }
+        }
+    }
+
     private static Document httpGetAndParseHtml(String link) throws IOException {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
+        CloseableHttpClient httpclient = HttpClients.custom().setDefaultRequestConfig(RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build()).build();
         HttpGet httpGet = new HttpGet(link);
+        httpGet.setHeader("user-agent", USERAGENT);
         try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
             System.out.println(response.getStatusLine());
             HttpEntity entity = response.getEntity();
