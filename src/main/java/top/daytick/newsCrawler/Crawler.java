5fdb038ca1f0b27f3b3da6cbead54b6bd070f310
luyi <luyi95@foxmail.com>
2020-03-29 21:56:58 +0800

    Finish database operation
    use mysql instead of collections

--- src/main/java/top/daytick/newsCrawler/Crawler.java
+++ src/main/java/top/daytick/newsCrawler/Crawler.java
@@ -13,55 +13,113 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.sql.*;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Crawler {
-    private static final Pattern PATTERN = Pattern.compile("https?://\\w*\\.?sina\\.cn.*");
+    private static final Pattern PATTERN = Pattern.compile("https?://(news\\.)?sina\\.cn.*");
     private static final String USERAGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36";
 
-    public static void main(String[] args) throws IOException {
-        Queue<String> linkPool = new LinkedList<>();
-        Set<String> processedPool = new HashSet<>();
-        linkPool.offer("https://sina.cn");
+    public static void main(String[] args) throws IOException, SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/news?characterEncoding=utf-8", "root", "root");
 
-        while (true) {
-            if (linkPool.isEmpty()) {
-                break;
-            }
-
-            String link = linkPool.poll();
-            if (link.startsWith("//")) {
-                link = "https:" + link;
-            }
-            System.out.println("link=" + link);
-
-            if (processedPool.contains(link)) {
+        String link;
+        while ((link = getNextLinkThenDelete(connection)) != null) {
+            if (isLinkProcessed(connection, link)) {
                 continue;
             }
 
+            System.out.println("link=" + link);
             Matcher matcher = PATTERN.matcher(link);
             if (matcher.matches()) {
+
                 Document doc = httpGetAndParseHtml(link);
 
-                doc.select("a").stream().map(aTag -> aTag.attr("href")).filter(mayBeUsefulLink -> mayBeUsefulLink.startsWith("http")).forEach(linkPool::offer);
+                parseLinksFromPageAndStoreIntoDatabase(connection, doc);
 
-                storeIntoDatabaseIfItIsNews(doc);
+                storeIntoDatabaseIfItIsNews(connection, doc, link);
             }
 
-            processedPool.add(link);
-            System.out.print("\n");
+            updateDatabase(connection, link, "INSERT INTO links_already_processed(link) VALUES(?)");
         }
 
     }
 
-    private static void storeIntoDatabaseIfItIsNews(Document doc) {
+    private static void parseLinksFromPageAndStoreIntoDatabase(Connection connection, Document doc) throws SQLException {
+        List<Element> aTags = doc.select("a");
+        if (!aTags.isEmpty()) {
+            for (Element aTag : aTags) {
+                String link = aTag.attr("href");
+
+                if (link.startsWith("//")) {
+                    link = "https:" + link;
+                }
+
+                if (link.startsWith("http")) {
+                    updateDatabase(connection, link, "INSERT INTO links_to_be_processed(link) VALUES(?)");
+                }
+            }
+        }
+    }
+
+    private static boolean isLinkProcessed(Connection connection, String link) throws SQLException {
+        ResultSet resultSet = null;
+        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT link FROM links_already_processed WHERE link = ?")) {
+            preparedStatement.setString(1, link);
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return true;
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+        return false;
+    }
+
+    private static String getNextLinkThenDelete(Connection connection) throws SQLException {
+        String link = getNextLink(connection);
+        if (link != null) {
+            updateDatabase(connection, link, "DELETE FROM links_to_be_processed WHERE link = ?");
+        }
+
+        return link;
+    }
+
+    private static void updateDatabase(Connection connection, String link, String sql) throws SQLException {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, link);
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    private static String getNextLink(Connection connection) throws SQLException {
+        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT link FROM links_to_be_processed LIMIT 1"); ResultSet resultSet = preparedStatement.executeQuery()) {
+            if (resultSet.next()) {
+                return resultSet.getString(1);
+            }
+        }
+        return null;
+    }
+
+    private static void storeIntoDatabaseIfItIsNews(Connection connection, Document doc, String link) throws SQLException {
         List<Element> articleTags = doc.select("article");
         if (!articleTags.isEmpty()) {
             for (Element articleTag : articleTags) {
                 String title = articleTag.child(0).text();
-                System.out.println("title=" + title + "\"");
+                List<Element> paragraphs = articleTag.select("p");
+                String content = paragraphs.stream().map(Element::text).collect(Collectors.joining("\n"));
+
+                try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO news(title, content, link, created_at, modified_at) VALUES(?, ?, ?, NOW(), NOW())")) {
+                    preparedStatement.setString(1, title);
+                    preparedStatement.setString(2, content);
+                    preparedStatement.setString(3, link);
+                    preparedStatement.executeUpdate();
+                }
             }
         }
     }
@@ -71,7 +129,6 @@ public class Crawler {
         HttpGet httpGet = new HttpGet(link);
         httpGet.setHeader("user-agent", USERAGENT);
         try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
-            System.out.println(response.getStatusLine());
             HttpEntity entity = response.getEntity();
             return Jsoup.parse(EntityUtils.toString(entity));
         }
--- src/main/resources/db/migration/V1__Create_tables.sql
+++ src/main/resources/db/migration/V1__Create_tables.sql
@@ -2,18 +2,18 @@
 CREATE TABLE news(
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(100),
-    content TEXT,
-    link VARCHAR(1000),
+    content MEDIUMTEXT,
+    link TEXT,
     created_at TIMESTAMP DEFAULT NOW(),
     modified_at TIMESTAMP DEFAULT NOW()
 )ENGINE=INNODB DEFAULT CHARSET=UTF8;
 
 -- 待处理的链接表
 CREATE TABLE links_to_be_processed(
-    link VARCHAR(1000)
-);
+    link TEXT
+)ENGINE=INNODB DEFAULT CHARSET=UTF8;
 
 -- 已经处理的链接表
 CREATE TABLE links_already_processed(
-    link VARCHAR(1000)
-);
+    link TEXT
+)ENGINE=INNODB DEFAULT CHARSET=UTF8;
